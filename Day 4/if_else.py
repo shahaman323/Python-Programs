ff94c4438acd9400652cedfe404974f830e57617
Aman Shah <shahaman323@gmail.com>
2021-06-16 20:58:19 +0530

    if_else_elif

---
+++ Day 4/if_else.py
@@ -0,0 +1,15 @@
+#using if else elif
+x=644
+y=444
+if y>x:
+    print("y is greater than x")
+elif x==y:
+    print("x and y are equal")
+else:
+    print("x is greater than y")
+
+#short hand if
+if x>y: print("x is greater than y")
+
+#short hand if else
+print("y") if x<y else print("x")
