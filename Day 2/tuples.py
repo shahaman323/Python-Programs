a47fd9ac64f105f5e18bd35e2734717162f1f007
Aman Shah <shahaman323@gmail.com>
2021-06-16 16:20:08 +0530

    Update tuples.py

--- Day 2/tuples.py
+++ Day 2/tuples.py
@@ -3,5 +3,12 @@ fixed_price = (22,33,44,66,55)
 print(fixed_price)
 
 #lets try for mutable and see
-fixed_price[1]=99
-print(fixed_price)
+#fixed_price[1]=99
+#print(fixed_price)
+
+
+#creating with one tuple
+my_tuple=("aman",)
+print(my_tuple)
+
+print(len(my_tuple))
