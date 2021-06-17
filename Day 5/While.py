13fb4ef0965a10d370749626b705d11e29148db0
Aman Shah <shahaman323@gmail.com>
2021-06-17 05:50:30 +0530

    while loop

---
+++ Day 5/While.py
@@ -0,0 +1,31 @@
+#while loop
+i=6
+while i>=1:
+    print(i)
+    i-=1
+
+#break statement
+print("break statement")
+x=6
+while x>=1:
+    print(x)
+    if x==3:
+        break
+    x-=1
+
+#continue
+##print("using continue")
+##y=6
+##while y>=1:
+##    y-=1
+##    if y==3:
+##        continue
+##    print(y)
+print("continue statement")
+y = 0
+while y < 6:
+  y += 1
+  if y == 3:
+    continue
+  print(y)         
+    
